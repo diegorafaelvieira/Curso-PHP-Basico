95f3429280aafa1a7ab3843c520510982964e9ff
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2021-04-24 02:56:44 -0300

    update mensagem do update

--- 28-crud/includes/mensagem.php
+++ 28-crud/includes/mensagem.php
@@ -6,8 +6,8 @@ if(isset($_SESSION['mensagem'])): ?>
 <script>
 	// Mensagem
 	window.onload = function() {
-		  /*M.toast({html: '<?php echo $_SESSION['mensagem']; ?>'});*/
-      alert("<?php echo $_SESSION['mensagem']; ?>");  
+		  M.toast({html: '<?php echo $_SESSION['mensagem']; ?>'});
+      
 	};
 </script>
 
